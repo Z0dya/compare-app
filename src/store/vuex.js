1be24406f76e1e39c51bc90d41312dcc420b8bcd
Лазуткин Владислав <vladislavlazutkin64@gmail.com>
2022-11-27 21:15:33 +0300

    create vuex store

--- src/store/vuex.js
+++ src/store/vuex.js
@@ -4,14 +4,38 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  },
+	state: {
+		files: {
+			file1: {
+				file: undefined,
+				table: {
+					rows: [],
+				},
+			},
+			file2: {
+				file: undefined,
+				table: {
+					rows: [],
+				},
+			},
+		},
+	},
+	// получать состояния из state ()
+	getters: {
+		file1(state) {
+			return state.files.file1;
+		},
+		file2(state) {
+			return state.files.file2;
+		},
+	},
+	// изменение состояний \ присваивание
+	mutations: {
+		// устанавливать информацию в  один из файлов
+		setFileData(state, payload) {
+			state.files[payload.fileNumber] = payload.newFileData;
+		},
+	},
+	actions: {},
+	modules: {},
 });
