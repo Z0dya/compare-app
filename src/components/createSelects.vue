11dd192515e8588422a8cde8785430dc3c7d5f32
Лазуткин Владислав <vladislavlazutkin64@gmail.com>
2022-11-29 20:41:35 +0300

    data in options

--- src/components/createSelects.vue
+++ src/components/createSelects.vue
@@ -1,16 +1,33 @@
 <template>
-	<select>
-		<option value="Unset">Не выбрано</option>
-		<option v-for="option in options" v-bind:key="option">{{ option }}</option>
-	</select>
+    <li>
+        <select v-model="atribute" v-on:change="optionSubmit">
+            <option value="Unset">Не выбрано</option>
+            <option v-for="option in options" v-bind:key="option">
+                {{ option }}
+            </option>
+        </select>
+    </li>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
 export default {
-	props: {
-		options: Array,
-	},
+    data() {
+        return {
+            atribute: 'Unset',
+        };
+    },
+    computed: mapGetters(['file1', 'file2']),
+    methods: {
+        optionSubmit() {
+            // console.log(this.atribute);
+            // console.log(this.file1.table.rows[0][`${this.option} `]);
+        },
+    },
+    props: {
+        options: Array,
+    },
 };
 </script>
 
-<style></style>
+<style lang="scss"></style>
